86a20f2ed74861e2d062577aa8f536882e2d83ab
Mehroj-SK <115388393+Mehroj-SK@users.noreply.github.com>
2022-12-04 01:04:22 +0530

    Add files via upload

---
+++ tuplePractice_1.py
@@ -0,0 +1,5 @@
+import sys
+a_list = ['abc', 'xyz', 123, 231, 13.31, 0.1312]
+a_tuple = ('abc', 'xyz', 123, 231, 13.31, 0.1312)
+print('The list size:', sys.getsizeof(a_list), 'bytes')
+print('The tuple size:', sys.getsizeof(a_tuple), 'bytes')
